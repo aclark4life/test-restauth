a58cb1be1caac88dcf88eca7201321fcc19c3c37
Alex Clark <aclark@aclark.net>
2022-05-11 11:44:26 -0400

    Update

--- demo/settings.py
+++ demo/settings.py
@@ -144,9 +144,6 @@ INTERNAL_IPS = [
 ]
 
 AUTHENTICATION_BACKENDS = (
- # Needed to login by username in Django admin, regardless of `allauth`
  "django.contrib.auth.backends.ModelBackend",
-
- # `allauth` specific authentication methods, such as login by e-mail
  "allauth.account.auth_backends.AuthenticationBackend",
 )
